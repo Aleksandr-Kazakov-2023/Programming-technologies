6d8ef0a8349f2a24acd6b9275137acfe01ff6344
Андрей Боев <a.boev.it@gmail.com>
2023-11-17 16:21:08 +0300

    Lab5 (Начало)

---
+++ Lab5/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lab5;
+
+public class Student
+{
+    public Student(Dictionary<string, int> points, string lastName, string firstName, string middleName, string gender, Calendar birthDate, string group, int course)
+    {
+        Points = points;
+        LastName = lastName;
+        FirstName = firstName;
+        MiddleName = middleName;
+        Gender = gender;
+        BirthDate = birthDate;
+        Group = group;
+        Course = course;
+    }
+
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string Gender { get; set; }
+    public Calendar BirthDate { get; set; }
+    public string Group { get; set; }
+    public int Course { get; set; }
+    public Dictionary<string, int> Points { get; set; }
+}
