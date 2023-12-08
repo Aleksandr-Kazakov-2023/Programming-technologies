e9e9072ffab4fb280c865a92cd2ba5c3fae4caec
Андрей Боев <a.boev.it@gmail.com>
2023-12-08 13:29:09 +0300

    Lab7

---
+++ Lab7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Polygon p = new Polygon();
+            p.LoadFromFile("..\\..\\polygon.pg");
+            Console.WriteLine(p.HasSelfIntersections() ? "Самопересекающийся" : "Не самопересекающийся");
+        }
+    }
+}
---
+++ Lab7Test/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Lab7;
+using NUnit.Framework;
+
+namespace Lab7Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestTrue()
+        {
+            Polygon p = new Polygon();
+            p.LoadFromFile("..\\..\\polygon1.pg");
+            Assert.False(p.HasSelfIntersections());
+        }
+        [Test]
+        public void TestFalse()
+        {
+            Polygon p = new Polygon();
+            p.LoadFromFile("..\\..\\polygon2.pg");
+            Assert.True(p.HasSelfIntersections());
+        }
+    }
+}
